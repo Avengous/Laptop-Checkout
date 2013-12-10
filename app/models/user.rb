cc71b8325e0b4c8bba67b618d14256e4a47e62d4
Avengous <xxstryderxx@hotmail.com>
2013-12-10 11:08:22 -0800

    Added User first_name, last_name and email validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
+  regex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/
+  validates :first_name,
+            :presence => true,
+            :uniqueness => { :case_sensitive => false }
+  validates :last_name,
+            :presence => true,
+            :uniqueness => { :case_sensitive => false }
+  validates_format_of :email,
+            :multiline => true,
+            :with => regex,
+            :uniqueness => { :case_sensitive => false }
   def full_name
     "#{first_name} #{last_name}"
   end
