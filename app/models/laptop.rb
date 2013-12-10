7e7a78870a0e37b20257ad8ebadeb0ad0b9cceb3
Avengous <xxstryderxx@hotmail.com>
2013-12-10 11:17:01 -0800

    Added Laptop and Transaction Validations.

--- app/models/laptop.rb
+++ app/models/laptop.rb
@@ -1,3 +1,8 @@
 class Laptop < ActiveRecord::Base
-
+  validates :scan_code,
+            :presence => true,
+            :uniqueness => { :case_sensitive => true }
+  validates :item_name,
+            :presence => true,
+            :uniqueness => { :case_sensitive => false }
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,6 @@
 class Transaction < ActiveRecord::Base
+  validates :laptops_id,
+            :presence => true
+  validates :users_id,
+            :presence => true
 end
